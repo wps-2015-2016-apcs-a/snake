eba9d99d280ce7c990f37f4f0d1ad28a2603743b
Guru Ramanathan <vguru.ramanathan@gmail.com>
2016-06-07 10:01:53 -0400

    Initial add of Keyboard.java (in main & test).

---
+++ src/test/java/snake/Keyboard.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ * @author Guru Ramanathan & Felicia Fung
+ * 
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Keyboard
+{
+  int x = 0;
+  int y = 0;
+}
+public static void main (String [] args)
+{
+  Scanner kb = new Scanner(System.in);
+  int upKey = kb.(System.input (JButton Up));
+  int rightKey = kb.(System.input (JButton Right));
+  int leftKey = kb.(System.input (JButton Left));
+  int downKey = kb.(System.input (JButton Down));
+}
