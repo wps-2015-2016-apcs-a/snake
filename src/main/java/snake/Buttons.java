af6c45f3a837bd335217c048f16d4f1b0adc9c4a
Misha Pilipchuk <medved@pilipchuk.org>
2016-06-11 14:07:50 -0400

    For snaketimer we added documentation, new methods. Buttons now compiles without having to comment the whole thing out.

--- src/main/java/snake/Buttons.java
+++ src/main/java/snake/Buttons.java
@@ -1,36 +1,52 @@
-/*
+/**
  * Buttons.java
  *
- * @author 2015-2016 APCS A-Block
+ * @author APCS A Block 2015-2016
+ * @authors Misha Pilipchuk and Nathan Li
+ *
  */
+
 package snake;
 
-public class Buttons  {
-/*//Replace with actual symbols
-private JButton Up = new JButton("Up");
-private JButton Right = new JButton("Right");
-private JButton Left = new JButton("Left");
-private JButton Down = new JButton("Down");
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-Up.addActionListener( new ActionListener() { 
-public void actionPerformed(ActionEvent e) { 
-System.out.println("Turn upwards"); } 
+public class Buttons {
+
+    ///////////////////////////FIELD///////////////////////////////////////
+    private JButton Up = new JButton("Up");
+    private JButton Right = new JButton("Right");
+    private JButton Left = new JButton("Left");
+    private JButton Down = new JButton("Down");
+
+    /////////////////////////////////CONSTRUCTORS////////////////////////////////////
+    public Buttons() {
+        Up.addActionListener(new ActionListener() {
+                                 public void actionPerformed(ActionEvent e) {
+                                     System.out.println("Turn upwards");
+                                 }
+                             }
+        );
+        Right.addActionListener(new ActionListener() {
+                                    public void actionPerformed(ActionEvent e) {
+                                        System.out.println("Turn right");
+                                    }
+                                }
+        );
+        Left.addActionListener(new ActionListener() {
+                                   public void actionPerformed(ActionEvent e) {
+                                       System.out.println("Turn left");
+                                   }
+                               }
+        );
+        Down.addActionListener(new ActionListener() {
+                                   public void actionPerformed(ActionEvent e) {
+                                       System.out.println("Turn down");
+                                   }
+                               }
+        );
+    }
 }
-);
-Right.addActionListener( new ActionListener() { 
-public void actionPerformed(ActionEvent e) { 
-System.out.println("Turn right"); } 
-}
-);
-Left.addActionListener( new ActionListener() { 
-public void actionPerformed(ActionEvent e) { 
-System.out.println("Turn left"); } 
-}
-);
-Down.addActionListener( new ActionListener() { 
-public void actionPerformed(ActionEvent e) { 
-System.out.println("Turn down"); } 
-}
-);
-*/
-}
+
+
