d67d4cc787e8dc9ac8d56ae3479dbdcefabb24f6
wps-dpetty <dpetty@winchesterps.org>
2016-06-15 09:41:10 -0400

    Fixed Keyboard to respond to keyPressed events. The Snake moves!

--- src/main/java/snake/Keyboard.java
+++ src/main/java/snake/Keyboard.java
@@ -12,32 +12,31 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Keyboard implements KeyListener {
-    /** Handle the key typed event from the text field. */
-    public void keyTyped(KeyEvent e) {
-        char id = e.getKeyChar();
-        System.out.printf("%s ", id);
+    /** Handle the key pressed event from the text field. */
+    public void keyPressed(KeyEvent e) {
+        int id = e.getKeyCode();
         switch(id) {
-          case KeyEvent.VK_UP:
-            Game.getSnake().turnUp();
-             break;
-          case KeyEvent.VK_DOWN:
-            Game.getSnake().turnDown();
-            break;
-          case KeyEvent.VK_LEFT:
-            Game.getSnake().turnLeft();
-            break;
-          case KeyEvent.VK_RIGHT:
-            Game.getSnake().turnRight();
-            break;
+            case KeyEvent.VK_UP:
+                Game.getSnake().turnUp();
+                break;
+            case KeyEvent.VK_DOWN:
+                Game.getSnake().turnDown();
+                break;
+            case KeyEvent.VK_LEFT:
+                Game.getSnake().turnLeft();
+                break;
+            case KeyEvent.VK_RIGHT:
+                Game.getSnake().turnRight();
+                break;
             default:
                 break;
         }
     }
 
-    /** Handle the key pressed event from the text field. */
-    public void keyPressed(KeyEvent e) {
-    }
     /** Handle the key released event from the text field. */
     public void keyReleased(KeyEvent e) {
+    }
+    /** Handle the key typed event from the text field. */
+    public void keyTyped(KeyEvent e) {
     }
 }
