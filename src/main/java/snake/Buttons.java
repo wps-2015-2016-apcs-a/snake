2c890ff54659c9b0f7d7cd2cf77b8b76b86a016e
Misha Pilipchuk <medved@pilipchuk.org>
2016-06-15 08:54:11 -0400

    Cleared out the old Buttons. Now it deals with starting the new game

--- src/main/java/snake/Buttons.java
+++ src/main/java/snake/Buttons.java
@@ -2,7 +2,7 @@
  * Buttons.java
  *
  * @author APCS A Block 2015-2016
- * @authors Misha Pilipchuk and Nathan Li
+ * @authors Misha Pilipchuk
  *
  */
 
@@ -12,54 +12,23 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Buttons {
+public class Buttons implements ActionListener{
+
 
     ///////////////////////////FIELD///////////////////////////////////////
-    private JButton Up = new JButton("Up");
-    private JButton Right = new JButton("Right");
-    private JButton Left = new JButton("Left");
-    private JButton Down = new JButton("Down");
+    private JButton y;
+    private JButton n;
 
     /////////////////////////////////CONSTRUCTORS////////////////////////////////////
     public Buttons() {
-        Up.addActionListener(new ActionListener() {
-                                 public void actionPerformed(ActionEvent e) {
-//                                     Snake.move();
-                                 }
-                             }
-        );
-        Right.addActionListener(new ActionListener() {
-                                    public void actionPerformed(ActionEvent e) {
-//                                     Snake.move();
-                                    }
-                                }
-        );
-        Left.addActionListener(new ActionListener() {
-                                   public void actionPerformed(ActionEvent e) {
-//                                     Snake.move();
-                                   }
-                               }
-        );
-        Down.addActionListener(new ActionListener() {
-                                   public void actionPerformed(ActionEvent e) {
-//                                     Snake.move();
-                                   }
-                               }
-        );
+
+        y = new JButton("New Game");
+        y.addActionListener(this);
     }
 
     /////////////////////////////////////////METHODS//////////////////////////////////////
+    public void actionPerformed(ActionEvent e) {
 
-    private void tell()
-    {
-        System.out.println("THERE IS NO USE FOR THIS CLASS");
-    }
-
-
-    public static void main(String[] args)
-    {
-        Buttons a = new Buttons();
-        a.tell();
     }
 }
 
