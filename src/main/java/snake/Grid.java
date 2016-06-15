37d67018490f296672afeab51c0cee9609331cda
wps-dpetty <dpetty@winchesterps.org>
2016-06-15 09:18:46 -0400

    Completed the merge of the last check-in for Grid.

--- src/main/java/snake/Grid.java
+++ src/main/java/snake/Grid.java
@@ -10,39 +10,21 @@ public class Grid {
     private int width, height;
     /* RED_FLAG: this is just a STUB to get things to compile. */
     public Grid(int width, int height) { this.width = width; this.height = height; }
-<<<<<<< HEAD
 
-   
-    
     ///////////////////FIELDS/////////////////////
-    
+
     public boolean testWall() {
+        // RED_FLAG: where did these numbers come from?!
         if (Game.getSnake().findMoveCoordinate().getRow() == 0 || Game.getSnake().findMoveCoordinate().getRow() == 1 || 
             Game.getSnake().findMoveCoordinate().getRow() == 98 || Game.getSnake().findMoveCoordinate().getRow() == 99)
-                return true;
+            return true;
+        // RED_FLAG: where did these numbers come from?!
         if (Game.getSnake().findMoveCoordinate().getCol() == 0 || Game.getSnake().findMoveCoordinate().getCol() == 1 ||
             Game.getSnake().findMoveCoordinate().getCol() == 73 || Game.getSnake().findMoveCoordinate().getCol() == 74)
-                return true;
-=======
-   
-    
-    ///////////////////FIELDS/////////////////////
-    public static final int HEIGHT = 900;
-    public static final int WIDTH = 1200;
-    public static final int SIZE = 8;
-    
-   
-    public boolean testWall{
-        if (findMoveCoordinate().getRow() == 0 || findMoveCoordinate().getRow() == 1 || 
-            findMoveCoordinate().getRow() == 98 || findMoveCoordinate().getRow() == 99)
             return true;
-        if (findMoveCoordinate().getCol() == 0 || findMoveCoordinate().getCol() == 1 ||
-            findMoveCoordinate().getCol() == 73 || findMoveCoordinate().getCol() == 74)
-            return true;
->>>>>>> db390c258915cc726f7cee3ad9c5c528636dc513
         return false;
     }
-    
+
     public void addFood() {
         int rowFood = 2 +(int)(Math.random() * 96);
         int colFood = 2 +(int)(Math.random() * 72);
