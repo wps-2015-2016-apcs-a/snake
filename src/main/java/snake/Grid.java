9edefa6e252d6a12a4e41c43284e180f70d0a77c
Ryan Delaney <rcdelaney0@gmail.com>
2016-06-16 10:02:17 -0400

    Changes to Grid.java

--- src/main/java/snake/Grid.java
+++ src/main/java/snake/Grid.java
@@ -9,31 +9,39 @@ package snake;
 public class Grid {
     private int width, height;
     /* RED_FLAG: this is just a STUB to get things to compile. */
-    public Grid(int width, int height) { this.width = width; this.height = height; }
+    public Grid(int width, int height) { this.width = width; this.height = height; addFood();}
     
     ///////////////////FIELDS/////////////////////
     private Coordinate food;
     
+    public int getGridHeight(){
+        return Game.getWindow().getHeight() / Game.getWindow().SIDE;
+    }
+    public int getGridWidth(){
+        return Game.getWindow().getWidth() / Game.getWindow().SIDE;
+    }
+    
     public boolean testWall() {
         // RED_FLAG: where did these numbers come from?!
         if (Game.getSnake().findMoveCoordinate().getRow() == 0 || Game.getSnake().findMoveCoordinate().getRow() == 1 || 
-            Game.getSnake().findMoveCoordinate().getRow() == 98 || Game.getSnake().findMoveCoordinate().getRow() == 99)
+            Game.getSnake().findMoveCoordinate().getRow() == getGridWidth() - 2 || Game.getSnake().findMoveCoordinate().getRow() == getGridWidth() -  1)
             return true;
         // RED_FLAG: where did these numbers come from?!
         if (Game.getSnake().findMoveCoordinate().getCol() == 0 || Game.getSnake().findMoveCoordinate().getCol() == 1 ||
-            Game.getSnake().findMoveCoordinate().getCol() == 73 || Game.getSnake().findMoveCoordinate().getCol() == 74)
+            Game.getSnake().findMoveCoordinate().getCol() == getGridHeight() - 2 || Game.getSnake().findMoveCoordinate().getCol() == getGridHeight() -1)
             return true;
         return false;
     }
 
     public void addFood() {
-        int rowFood = 2 +(int)(Math.random() * 96);
-        int colFood = 2 +(int)(Math.random() * 72);
+        int rowFood = 2 +(int)(Math.random() * getGridHeight() - 4);
+        int colFood = 2 +(int)(Math.random() * getGridWidth() - 4);
         Coordinate food = new Coordinate (rowFood, colFood);
-        for (int i = 0; i < Game.getSnake().getScore(); i++){
-            if (Game.getSnake().contains(food))
-                addFood();
-        }
+        if (Game.getSnake().contains(food))
+            addFood();
+        else 
+            this.food = food;
+                
     }
     public boolean isFood(Coordinate coord) {
         if (coord == food)
