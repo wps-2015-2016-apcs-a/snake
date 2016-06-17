b1ca88501c4bc0d91f33d8ca1fed4c14b658880e
Misha Pilipchuk <medved@pilipchuk.org>
2016-06-17 11:45:48 -0400

    Took all of its functions away and put them into window

--- src/main/java/snake/Buttons.java
+++ src/main/java/snake/Buttons.java
@@ -3,34 +3,27 @@
  *
  * @author APCS A Block 2015-2016
  * @author Misha Pilipchuk
+ * @author Nathan Li
  *
+ *
+ *
+ * EVERYTHING THAT WAS HERE IS NO LONGER HERE. IT WAS MOVED TO WINDOW (AS WE WERE TOLD TO DO)
  */
 
 package snake;
-
-import javax.swing.*;
-
-
 
 
 public class Buttons{
 
 
     ///////////////////////////FIELD///////////////////////////////////////
-    private String scoreKeep;
-    private String instructions;
-    private JTextField scoreBox;
-    private JTextField instructionBox;
+
 
     /////////////////////////////////CONSTRUCTORS////////////////////////////////////
     public Buttons(){
-        scoreKeep = "Score: " + Game.getSnake().getScore();
-        instructions = "Press space bar to start and pause, r to restart, arrows to move";
-        scoreBox = new JTextField(scoreKeep);
-        instructionBox = new JTextField(instructions);
     }
 
     /////////////////////////////////////////METHODS//////////////////////////////////////
-    public JTextField getScoreBox(){ return scoreBox;}
-    public JTextField getInstructionBox() { return instructionBox;}
+
+
 }
